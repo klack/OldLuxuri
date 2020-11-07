5027ce6750d6517011b75228f6b42e8d1d7e3264
Richard Layton <rlayton@gmail.com>
2020-11-07 00:07:08 -0700

    Queue working cool...

--- scripts/commandQueue.cs
+++ scripts/commandQueue.cs
@@ -1,10 +1,23 @@
-spstr commandQueue.txt,vars.s.txt,"\r",0
-//get the lenght of the command
-strlen vars.s.txt,j
-//get the length of the command queue
-strlen commandQueue.txt,k
-//calc the amount of chars to copy
-l=k-j
-//remove the command from the queue by getting a substring of the commandqueue
-prints vars.s.txt,0
-substr commandQueue.txt,commandQueue.txt,j+2,l //+1 to count \r
+strlen commandQueue.txt,k //get the length of the command queue
+while(k>0)
+{
+  click readBuffer,1
+  if(ok==1)
+  {
+    spstr commandQueue.txt,vars.s.txt,"\r",0
+    if(vars.s.txt!="")
+    {
+      strlen vars.s.txt,j //get the lenght of the command
+      l=k-j //calc the amount of chars to copy
+      //remove the command from the queue by getting a substring of the commandqueue
+      substr commandQueue.txt,commandQueue.txt,j+2,l //+2 to count \r
+      prints vars.s.txt,0
+      printh 0A
+      ok=0
+    }else
+    {
+      commandQueue.txt=""
+    }
+  }
+  strlen commandQueue.txt,k //get the length of the command queue
+}
