542193f1b6929cbcefe21cf17dbe0ecb9d4324af
Richard Layton <rlayton@gmail.com>
2020-11-10 17:44:04 -0700

    Still adjusting....

--- scripts/sendCommands.cs
+++ scripts/sendCommands.cs
@@ -11,46 +11,52 @@ if(busy==0)
 commands.command.txt=""
 
 //Send Queue
-OkBlocks=0
-strlen commands.queue.txt,k //get the length of the command queue
-while(k>0&&OkBlocks<20)
+if(ok==1)
 {
-  doevents
-  click readBuffer,1
-  if(ok==1)
+  OkBlocks=0
+  strlen commands.queue.txt,k //get the length of the command queue
+  while(k>0&&OkBlocks<50)
   {
-    OkBlocks=0
-    spstr commands.queue.txt,vars.s.txt,"\r",0
-    if(vars.s.txt!="")
+    doevents
+    click readBuffer,1
+    if(ok==1)
     {
-      strlen vars.s.txt,j //get the lenght of the command
-      l=k-j //calc the amount of chars to copy
-      //remove the command from the queue by getting a substring of the commandqueue
-      substr commands.queue.txt,commands.queue.txt,j+2,l //+2 to count \r
-      commands.command.txt=vars.s.txt
-      click sendCommand,1
+      OkBlocks=0
+      spstr commands.queue.txt,vars.s.txt,"\r",0
+      if(vars.s.txt!="")
+      {
+        strlen vars.s.txt,j //get the lenght of the command
+        l=k-j //calc the amount of chars to copy
+        //remove the command from the queue by getting a substring of the commandqueue
+        substr commands.queue.txt,commands.queue.txt,j+2,l //+2 to count \r
+        commands.command.txt=vars.s.txt
+        click sendCommand,1
+      }else
+      {
+        commands.queue.txt=""
+        QueueBlocks=0
+      }
     }else
     {
-      commands.queue.txt=""
-      QueueBlocks=0
+      click readBuffer,1
+      OkBlocks+=1
+      click debug,1
+      doevents
     }
+    strlen commands.queue.txt,k //get the length of the command queue
+  }
+  if(OkBlocks>=50)
+  {
+    QueueBlocks+=1
   }else
   {
-    click readBuffer,1
-    OkBlocks+=1
-    click debug,1
-    doevents
+    QueueBlocks=0
   }
-  strlen commands.queue.txt,k //get the length of the command queue
-}
-if(OkBlocks>=20)
-{
-  QueueBlocks+=1
 }else
 {
-  QueueBlocks=0
+  QueueBlocks+=1
 }
-if(QueueBlocks>20)
+if(QueueBlocks>10)
 {
   commands.command.txt="M114" //Maybe an ok was missed, try and trigger one
   click sendCommand,1
